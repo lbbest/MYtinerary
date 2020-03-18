737c710b419f4cd596e96608fe94b4d342126c03
Liam Best <liambbest@googlemail.com>
2020-03-18 12:29:57 +0100

    Post request setup complete

--- server/routes/cities.js
+++ server/routes/cities.js
@@ -5,13 +5,30 @@ const router = express.Router();
 const cityModel = require("../model/cityModel");
 
 /*get all cities*/
-router.get("/all",
-    (req, res) => {
-        cityModel.find({})
-            .then(files => {
-                res.send(files)
-            })
-            .catch(err => console.log(err));
-    });
+router.get("/all", (req, res) => {
+  cityModel
+    .find({})
+    .then(files => {
+      res.send(files);
+    })
+    .catch(err => console.log(err));
+});
 
 module.exports = router;
+
+/*cities post route*/
+router.post("/", (req, res) => {
+  const newCity = new cityModel({
+    name: req.body.name,
+    country: req.body.country,
+    img: req.body.img
+  });
+  newCity
+    .save()
+    .then(city => {
+      res.send(city);
+    })
+    .catch(err => {
+      res.status(500).send("Server error");
+    });
+});
