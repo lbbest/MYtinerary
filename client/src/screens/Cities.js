4c6ffc0d7cf0887fd71492bc49cfa4622f821c5e
Liam Best <liambbest@googlemail.com>
2020-03-18 11:43:05 +0100

    City Filter Complete

--- client/src/screens/Cities.js
+++ client/src/screens/Cities.js
@@ -6,7 +6,8 @@ export class Cities extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      cities: []
+      cities: [],
+      filter: ""
     };
   }
 
@@ -20,18 +21,51 @@ export class Cities extends Component {
 
   componentDidMount() {
     this.fetchCities();
+    this.timer = setInterval(() => this.fetchCities(), 60000);
   }
+
+  componentWillUnmount() {
+    this.timer = null;
+  }
+
+  handleFilter = e => {
+    this.setState({
+      filter: e.target.value
+    });
+  };
+
+  cityFilter = () => {
+    let filter = this.state.cities.filter(cities => {
+      return (
+        cities.country.includes(this.state.filter) ||
+        cities.name.includes(this.state.filter)
+      );
+    });
+    return filter;
+  };
 
   render() {
     console.log(this.state.cities);
+    const filteredCities = this.cityFilter();
+    console.log(filteredCities);
     return (
       <div>
         <Nav />
+        <div className="filter">
+          <input
+            type="text"
+            id="filter"
+            placeholder="Find a city..."
+            value={this.state.filter}
+            onChange={this.handleFilter}
+          ></input>
+        </div>
         <div className="content">
           <h1>Cities</h1>
-          {this.state.cities.map((city, index) => {
-            return <City key={index} city={city} />;
-          })}
+          {this.state.cities &&
+            filteredCities.map((city, index) => {
+              return <City key={index} city={city} />;
+            })}
         </div>
       </div>
     );
