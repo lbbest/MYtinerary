0fbea54ff877d8a9c1824b04ad6674c11da2df37
Liam Best <liambbest@googlemail.com>
2020-03-22 19:05:34 +0100

    Redux Complete (Sprint 3 Epic 1 Done)

--- client/src/screens/Cities.js
+++ client/src/screens/Cities.js
@@ -1,69 +1,32 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { getCityAction } from "../store/actions/cityActions";
 import Nav from "../components/Nav";
 import City from "../components/City";
 
 export class Cities extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cities: [],
-      filter: ""
-    };
+  constructor() {
+    super();
   }
-
-  fetchCities = () => {
-    this.setState({ ...this.state, isFetching: true });
-    fetch("http://localhost:5000/cities/all")
-      .then(response => response.json())
-      .then(result => this.setState({ cities: result, isFetching: false }))
-      .catch(e => console.log(e));
-  };
 
   componentDidMount() {
-    this.fetchCities();
-    this.timer = setInterval(() => this.fetchCities(), 60000);
+    this.props.getCityAction();
   }
-
-  componentWillUnmount() {
-    this.timer = null;
-  }
-
-  handleFilter = e => {
-    this.setState({
-      filter: e.target.value
-    });
-  };
-
-  cityFilter = () => {
-    let filter = this.state.cities.filter(cities => {
-      return (
-        cities.name.includes(this.state.filter) ||
-        cities.country.includes(this.state.filter)
-      );
-    });
-    return filter;
-  };
 
   render() {
-    const filteredCities = this.cityFilter();
+    const { cities } = this.props;
+    // console.log(this.props);
     return (
       <div>
         <Nav />
         <div className="filter">
-          <input
-            type="text"
-            id="filter"
-            placeholder="Find a city..."
-            value={this.state.filter}
-            onChange={this.handleFilter}
-          ></input>
+          <input type="text" id="filter" placeholder="Find a city..."></input>
         </div>
         <div className="content">
           <div id="cityList">
-            {this.state.cities &&
-              filteredCities.map((city, index) => {
-                return <City key={index} city={city} />;
-              })}
+            {cities.map((city, index) => {
+              return <City key={index} city={city} />;
+            })}
           </div>
         </div>
       </div>
@@ -71,4 +34,15 @@ export class Cities extends Component {
   }
 }
 
-export default Cities;
+const mapStateToProps = state => {
+  // console.log(state);
+  return {
+    cities: state.cities.data
+  };
+};
+
+const mapDispatchToProps = dispatch => ({
+  getCityAction: () => dispatch(getCityAction)
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Cities);
